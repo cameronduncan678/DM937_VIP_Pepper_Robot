aa208684f48bcdbe48de4e007d9a77caacc02157
rzahra5 <roisin.bafiq.2024@uni.strath.ac.uk>
2025-12-16 15:19:58 +0000

    Add head movement control script
    -Suleiman

---
+++ Scripts/move_head.py
@@ -0,0 +1,39 @@
+#! usrbinenv python
+# -- encoding UTF-8 --
+
+Example Use Dictionary TTS Methods (English)
+
+import qi
+import argparse
+import sys
+import time
+
+def main(session)
+    motion = session.service(ALMotion)
+
+    HEAD_POINT = [-0.197, 0.34]  # look towards pointed object
+
+    motion.setAngles(
+        [HeadYaw, HeadPitch],
+        HEAD_POINT,
+        0.15
+    )
+    time.sleep(1.0)
+
+
+if __name__ == __main__
+    parser = argparse.ArgumentParser()
+    parser.add_argument(--ip, type=str, default=10.1.32.138,
+                        help=Robot IP address. On robot or Local Naoqi use '10.1.32.138'.)
+    parser.add_argument(--port, type=int, default=9559,
+                        help=Naoqi port number)
+
+    args = parser.parse_args()
+    session = qi.Session()
+    try
+        session.connect(tcp + args.ip +  + str(args.port))
+    except RuntimeError
+        print (Can't connect to Naoqi at ip  + args.ip +  on port  + str(args.port) +.n
+               Please check your script arguments. Run with -h option for help.)
+        sys.exit(1)
+    main(session)
